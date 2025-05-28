77dd74c76026d84044cb55314ca6231409f65161
Umer Naveed <muhammadumernaveed1234@gmail.com>
2025-05-28 15:10:40 +0500

    first commit

---
+++ .next/static/chunks/src_app_cart_page_7333abe5.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_39f6ae26._.js"
+],
+    source: "dynamic"
+});
